80a928a7c8f5d8731923f374643ddad8ec255a90
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-02-21 16:31:29 +0700

    Update node server

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ import errorHandler from './app/middlewares/errorMiddleware.js'
 import expressEjsLayouts from 'express-ejs-layouts'
 import path from "path"
 import conn from './app/config/ConnectDB.js'
-// import db from './app/config/Database.js'
-// import Matkul from './app/models/MatkulModel.js'
+// import Hari from './app/models/HariModel.js'
+
 const app = express()
 
 // Connect to Database using mysql module
-
     conn.connect((err) => {
         if (err) return console.log(err.message)
         console.log('database is connected...')
     })
 
-
 // Connect to Database using Sequelize Module
 // try {
 //     await db.authenticate()
 //     console.log('Database is connected...')
-//     // await Matkul.sync()
+//     // await Hari.sync()
 // } catch (error) {
 //     console.log('Database is not connected!', error.message)
 // }
