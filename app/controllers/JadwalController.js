63d195eb276c8e2e0cc8417135eb186352610558
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-03-09 17:02:13 +0700

    Update jadwal controller and model

--- app/controllers/JadwalController.js
+++ app/controllers/JadwalController.js
@@ -1,53 +1,92 @@
-import sequelize from 'sequelize'
-
-// jenis ruang kelas
-var praktikum = 'praktek'
-var teori = 'teori'
-var lab = 'laboratorium'
-
-// buat popuiasi
-var jenis_semester
-var tahun_akademik
-var ukuranPopulasi
-var crossOver
-var mutasi
-
-var pengampu = new Array()
-var individu
-var sks
-var dosen
-
-var jam
-var hari
-var id_dosen
-
-// waktu permintaan
-var waktu_dosen
-var jenis_mk
-
-var ruangLab
-var ruangKelas
-var logAmbilData
-var logInisialisasi
-
-var log
-var induk
-
-// if jum'at
-var kode_jumat
-var range_jumat
-var kode_dhuhur
-var is_notReady
-
+import conn from "../config/ConnectDB.js"
+import GeneticAlgorithm from "../config/GeneticAlgorithm.js"
+import { getResult, getRuangLab, getRuangReg, resData } from "../models/JadwalModel.js"
+import { getData, initialization } from "../config/GA.js"
 
 export const getJadwal = async (req, res) => {
     try {
+        const result = await getResult(res)
+
         res.render('pagejadwal/menujadwal', {
             title: 'Menu Jadwal Kuliah',
-            layout: 'layouts/templates'
+            layout: 'layouts/templates',
+            result
         })
+        console.log(result)
         res.status(200)
     } catch (error) {
         res.status(500).json({msg: error.message})
     }
+}
+
+export const processPenjadwalan = async (req, res) => {
+    try {
+        const {
+            jmlh_populasi,
+            probabilitas_crossover,
+            probabilitas_mutasi,
+            jmlh_generasi,
+            semester_tipe,
+            tahun_akademik
+        } = req.body
+        
+        const jadwal = await resData(semester_tipe, tahun_akademik)
+
+        if (jadwal === undefined) {
+            
+            res.send({msg: 'Tidak ada data Semester dan Tahun akademik! Data ini adalah data Sebelumnya'})
+        } else {
+            const myClass = new GeneticAlgorithm(semester_tipe, tahun_akademik, jmlh_populasi, probabilitas_crossover, probabilitas_mutasi, 5, '4-5-6', 6)
+
+            myClass.getData()
+            myClass.initialize()
+
+            let found = false
+
+            for (let i = 0; i < jmlh_generasi; i++) {
+                let fitness = await myClass.countFitness()
+
+                await myClass.selection(fitness)
+                await myClass.startCrossover()
+
+
+                let fitnessAfterMutation = await myClass.mutate()
+                // console.log(fitnessAfterMutation.length)
+                for (let j = 0; j < fitnessAfterMutation.length; j++) {
+                    if (fitnessAfterMutation[j] == 1) {
+                        conn.query("TRUNCATE TABLE t_jadwal", function (error, rows, fields) {
+                            if (error) throw error
+                        })
+
+                        // let jadwal_kuliah = [[]]
+                        let jadwal_kuliah = await myClass.getIndividu(j)
+                        console.log(jadwal_kuliah)
+
+                        for (let k = 0; k < jadwal_kuliah.length; k++) {
+                            // jadwal_kuliah[k] = []
+                            const id_pengampu = parseInt(jadwal_kuliah[k][0])
+                            const id_jam = parseInt(jadwal_kuliah[k][1])
+                            const id_hari = parseInt(jadwal_kuliah[k][2])
+                            const id_ruang = parseInt(jadwal_kuliah[k][3])
+                            conn.query("INSERT INTO t_jadwal(id_pengampu, id_jam, id_hari, id_ruang) VALUES (?, ?, ?, ?)", [id_pengampu, id_jam, id_hari, id_ruang], function (error, rows, fields) {
+                                if(error) throw error
+                            })
+                            // console.log(id_ruang)
+                        }
+                        found = true
+                    }
+                    if (found) break
+                }
+                if (found) break
+            }
+            if (!found) {
+                throw new Error('Tidak ditemukan solusi')
+            }
+        }
+        res.redirect('/jadwalkuliah')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/jadwalkuliah')
+    }
+
 }
--- app/models/JadwalModel.js
+++ app/models/JadwalModel.js
@@ -1,12 +1,74 @@
 import conn from "../config/ConnectDB.js"
 
-function getJadwal(req, res) {
+function getResult(req, res) {
     return new Promise((resolve, reject) => {
-        conn.query("SELECT e.nama as hari, Concat_WS('-', concat('(', g.kode), concat((SELECT kode FROM jam WHERE kode = (SELECT jm.kode FROM jam jm WHERE MID(jm.range_jam,1,5) = MID(g.range_jam,1,5)) + (c.sks - 1)),')')) as sesi, Concat_WS('-', MID(g.range_jam,1,5), (SELECT MID(range_jam,7,5) FROM jam WHERE kode = (SELECT jm.kode FROM jam jm WHERE MID(jm.range_jam,1,5) = MID(g.range_jam,1,5)) + (c.sks - 1))) as jam_kuliah, c.nama as nama_mk, c.sks as sks, c.semester as semester, b.kelas as kelas, d.nama as dosen, f.nama as ruang FROM jadwalkuliah a LEFT JOIN pengampu b ON a.kode_pengampu = b.kode LEFT JOIN matakuliah c ON b.kode_mk = c.kode LEFT JOIN dosen d ON b.kode_dosen = d.kode LEFT JOIN hari e ON a.kode_hari = e.kode LEFT JOIN ruang f ON a.kode_ruang = f.kode LEFT JOIN jam g ON a.kode_jam = g.kode order by e.kode asc,Jam_Kuliah asc")
-        if (error) return reject(error)
+        conn.query("SELECT e.hari as hari, Concat_WS('-', concat('(', g.id), concat((SELECT id FROM t_waktu WHERE id = (SELECT jm.id FROM t_waktu jm WHERE MID(jm.range_waktu,1,5) = MID(g.range_waktu,1,5)) + (c.sks - 1)),')')) as sesi, Concat_WS('-', MID(g.range_waktu,1,5), (SELECT MID(range_waktu,7,5) FROM t_waktu WHERE id = (SELECT jm.id FROM t_waktu jm WHERE MID(jm.range_waktu,1,5) = MID(g.range_waktu,1,5)) + (c.sks - 1))) as jam_kuliah, c.matkul as nama_mk, c.sks as sks, c.semester as semester, b.kelas as kelas, d.name as dosen, f.no_ruang as ruang FROM t_jadwal a LEFT JOIN t_pengampu b ON a.id_pengampu = b.id LEFT JOIN t_matkul c ON b.id_mk = c.id LEFT JOIN t_dosen d ON b.id_dosen = d.id LEFT JOIN t_hari e ON a.id_hari = e.id LEFT JOIN t_ruang f ON a.id_ruang = f.id LEFT JOIN t_waktu g ON a.id_jam = g.id order by e.id asc,Jam_Kuliah ASC", function (error, rows, field) {
+            if (error) throw error
 
-        return resolve(rows)
+            return resolve(rows)
+        })
     })
 }
 
-export default getJadwal
+function resData(semester_tipe, tahun_akademik) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT a.id, b.sks, a.id_dosen, b.jenis FROM t_pengampu a LEFT JOIN t_matkul b ON a.id_mk = b.id WHERE b.semester%2 = ? AND a.tahun_akademik = ?", [semester_tipe, tahun_akademik], function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+function getWaktu(req, res) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT id FROM t_waktu", function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+function getHari(req, res) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT id FROM t_hari", function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+function getRuangReg(req, res) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT id FROM t_ruang WHERE jenis='teori'", function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+function getRuangLab(req, res) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT id FROM t_ruang WHERE jenis='praktek'", function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+function getWaktuDosen(req, res) {
+    return new Promise((resolve, reject) => {
+        conn.query("SELECT kode_dosen, CONCAT_WS(':',kode_hari,kode_waktu) as kode_hari_jam FROM t_waktu_khusus", function (error, rows, fields) {
+            if (error) throw error
+
+            return resolve(rows)
+        })
+    })
+}
+
+
+export { getResult, resData, getWaktu, getHari, getRuangReg, getRuangLab, getWaktuDosen}
